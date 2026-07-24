a4a6df10211fbf5d948c1b6f913dcaf5412dbe7c
Abdulai059 <abdulaiosman8080@gmail.com>
2026-07-24 02:24:57 +0000

    adding authentication and authorisation

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,6 +1,12 @@
 import { catchAsync } from "../utils /catchAsync";
 import { Request, Response, NextFunction } from "express";
 import prisma from "../lib/prisma";
+import AppError from "../utils /AppError";
+
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+
 
 export const singUp = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -19,6 +25,34 @@ export const singUp = catchAsync(
       data: {
         user: newUser,
       },
+    });
+  },
+);
+
+export const login = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { phone, password } = req.body;
+
+    // 1) Check if phone and password exist
+    if (!phone || !password) {
+      return next(new AppError("Please provide phone and password", 400));
+    }
+
+    // 2) Check if user exists && password is correct
+    const user = await prisma.user.findUnique({
+      where: {
+        phone,
+      },
+    });
+
+    if (!user || user.passwordHash !== password) {
+      return next(new AppError("Incorrect phone or password", 401));
+    }
+
+    // 3) If everything ok, send token to client
+    res.status(200).json({
+      status: "success",
+      message: "Login successful",
     });
   },
 );
